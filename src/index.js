2217d3b98761edc14cc4352f5f41cfaefa25a07a
sanjeev45 <tro845k@gmail.com>
2024-01-12 10:25:28 +0530

    Completed basic sequelize for creating a db

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
-require('dotenv').config();
+const bodyParser = require('body-parser');
+const {PORT} =  require('./config/serverConfig');
 const setupAndStartServer = async () => {
     //create the express object
     const app = express();
-    const PORT = 3000;
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
     app.listen(PORT, () => {
         console.log(`Server is running at ${PORT}`);
-        console.log(process.env);
-    });
+        
+    
+    })
+
+   
 }
 
 setupAndStartServer();
